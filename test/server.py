077bff1a4eabaaabf75a864cb282ca9a27e5d36e
Simon Norberg <simon@dackbrann.net>
2009-03-09 10:50:32 +0100

    first commit

---
+++ test/server.py
@@ -0,0 +1,19 @@
+# Echo server program
+import socket
+
+HOST = ''                 # Symbolic name meaning all available interfaces
+PORT = 1337              # Arbitrary non-privileged port
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.bind((HOST, PORT))
+s.listen(1)
+conn, addr = s.accept()
+print 'Connected by', addr
+while 1:
+	data = conn.recv(1024)
+	if not data: break
+	print data
+	if data == "quit\r\n":
+		conn.send("bye")
+		break
+	conn.send(data)
+conn.close()
