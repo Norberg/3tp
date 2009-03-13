53945bf84ae31305d3d0c40928976f047929018c
Simon Norberg <simon@dackbrann.net>
2009-03-13 21:39:23 +0100

    server is now able to play complete game, if oponent send somewhat sane data

--- server.py
+++ server.py
@@ -1,3 +1,4 @@
+# This Python file uses the following encoding: utf-8
 import socket
 import random
 
@@ -30,14 +31,12 @@ class Server:
 		#Start set-play
 		self.gamestate = "Set"
 		self.setPlay()
-		while 1:
-			data = self.conn.recv(1024)
-			if not data: break
-			#self.parse(data)
-			if data == "quit\r\n":
-				break
-			self.conn.send(data)
+		#Start mov-play
+		self.gamestate = "Mov"
+		self.movPlay()
+
 		print self.clientHeader
+		print "Quiting.."
 		self.conn.close()
 
 	def reciveHeaders(self):
@@ -55,7 +54,9 @@ class Server:
 		if len(self.clientHeader) < 3 and data != "":
 			field = data.partition(":")[0]
 			value = data.partition(":")[2]
-	 		self.clientHeader[field] = value
+			if field == "name" or field == "version" or \
+			   field == "user-agent": 
+	 			self.clientHeader[field] = value
 
 	def sendHeaders(self):
 		data = ""
@@ -70,10 +71,14 @@ class Server:
 		for i in range(6):
 			if self.turn == 0: #servers tur
 				self.printBord()
+				self.printWinner()
 				pos = raw_input("set >")
 				x = pos.partition(",")[0]	
 				y = pos.partition(",")[2]
 				print "err:", self.set(x, y)
+				self.printBord()
+				self.printWinner()
+				print "Waiting..."
 				self.turn = 1	
 							
 			else: #clients tur
@@ -94,15 +99,59 @@ class Server:
 			return int(value)
 		else:
 			print "Server expected 'err' but got:", data
+	
+	def movPlay(self):
+		while 1:
+			if self.turn == 0: #servers tur
+				self.printBord()
+				self.printWinner()
+				pos = raw_input("from >")
+				fromX = pos.partition(",")[0]	
+				fromY = pos.partition(",")[2]
+				pos = raw_input("to >")
+				toX = pos.partition(",")[0]	
+				toY = pos.partition(",")[2]
+				print "err:", self.mov(fromX, fromY, toX, toY)
+				self.printBord()
+				self.printWinner()
+				print "Waiting..."
+				self.turn = 1	
+							
+			else: #clients tur
+				self.waitOnOponent()
+				self.turn = 0
+
+	def mov(self, fromX, fromY, toX, toY):
+		fromX = int(fromX)
+		fromY = int(fromY)
+		toX = int(toX)
+		toY = int(toY)
+		self.conn.send("mov:" + str(fromX) + "," + str(fromY) + \
+			       "→" + str(toX) + "," + str(toY) + CRLF)
+		data = self.conn.recv(8)
+		data.strip(CRLF)
+		field = data.partition(":")[0]
+		value = data.partition(":")[2]
+		if field == "err":
+			if int(value) == 0:
+				self.bord[toX-1][toY-1] = 1
+	       			self.bord[fromX-1][fromY-1] = 0
+			return int(value)
+		else:
+			print "Server expected 'err' but got:", data
 
 	def waitOnOponent(self):
 		data = self.conn.recv(16)
 		if self.gamestate == "Set":
-			data.strip(CRLF)
-			field = data.partition(":")[0]
-			value = data.partition(":")[2]
-			x = int(value.partition(",")[0]) - 1
-			y = int(value.partition(",")[2]) - 1
+			try:
+				data.strip(CRLF)
+				field = data.partition(":")[0]
+				value = data.partition(":")[2]
+				x = int(value.partition(",")[0]) - 1
+				y = int(value.partition(",")[2]) - 1
+			except:
+				self.waitOnOponent()
+				self.sendError(5)
 			if field == "set":
 				if x >= 3 or y >= 3: #pos not on border
 					self.sendError(2)
@@ -123,6 +172,92 @@ class Server:
 				self.sendError(5)
 				self.waitOnOponent()
 				return
+		elif self.gamestate == "Mov":
+			try:
+				data.strip(CRLF)
+				field = data.partition(":")[0]
+				value = data.partition(":")[2]
+				fromPos = value.partition("→")[0]
+				toPos = value.partition("→")[2]
+
+				fromX = int(fromPos.partition(",")[0]) - 1
+				fromY = int(fromPos.partition(",")[2]) - 1
+				toX = int(toPos.partition(",")[0]) - 1
+				toY = int(toPos.partition(",")[2]) - 1
+			except:
+				self.waitOnOponent()
+				self.sendError(5)
+			if field == "mov":
+				#pos not on border
+				if fromX >= 3 or fromY >= 3 \
+				or toX >= 3 or toY >= 3: 
+					self.sendError(2)
+					self.waitOnOponent()
+					return
+				#success
+				elif self.bord[toX][toY] == 0 and \
+				     self.bord[fromX][fromY] == 2:
+					self.sendError(0)
+					self.bord[toX][toY] = 2
+					self.bord[fromX][fromY] = 0
+					return
+				#not your marker
+				elif self.bord[fromX][fromY] != 2:
+					self.sendError(3)
+					self.waitOnOponent()
+					return
+				elif self.bord[toX][toY] != 0:
+					self.sendError(4)
+					self.waitOnOponent()
+					return
+					
+			#oponent dont follow protokoll
+			else:
+				self.sendError(5)
+				self.waitOnOponent()
+				return
+			
+	def printWinner(self):
+		winner = self.getWinner()
+		if winner == 1:
+			print "You won the game"
+			self.conn.send("win:0" + CRLF)
+			self.conn.close()
+			quit()
+		elif winner == 2:
+			print "You lost the game"
+			self.conn.send("win:1" + CRLF)
+			self.conn.close()
+			quit()
+
+
+	def getWinner(self):
+		for player in [1,2]:
+			#check vertical
+			for i in range(3):
+				if self.bord[i][0] == player and \
+				   self.bord[i][1] == player and \
+				   self.bord[i][2] == player:
+				   	return player
+			#check horisontal
+			for i in range(3):
+				if self.bord[0][i] == player and \
+				   self.bord[1][i] == player and \
+				   self.bord[2][i] == player:
+				   	return player
+			#check /
+			if self.bord[2][0] == player and \
+			   self.bord[1][1] == player and \
+			   self.bord[0][2] == player:
+			   	return player
+			#check \
+			if self.bord[0][0] == player and \
+			   self.bord[1][1] == player and \
+			   self.bord[2][2] == player:
+				return player
+
+		#no player have won yet
+		return 0
 
 	def markersInGame(self):
 		nr = 0
@@ -136,12 +271,23 @@ class Server:
 		self.conn.send("err:" + str(error) + CRLF)
 
 	def printBord(self):
-		for i in range(9):
-			if (self.bord[i/3][i%3] == 1):
-				print "X",
-			elif (self.bord[i/3][i%3] == 2):
-				print "O",
-			else:
-				print "_",
-			if (i % 3 == 0):
-				print
+		print "╭─┬─┬─╮"
+		for i in range(3):
+			print "│" + self.getMarker(self.bord[i][0]) + "│" \
+			+ self.getMarker(self.bord[i][1]) + "│" \
+			+ self.getMarker(self.bord[i][2]) + "│"
+			if i != 2:
+				print "├─┼─┼─┤"
+
+		print "╰─┴─┴─╯"
+
+	def getMarker(self, val):
+		if val == 0:
+			return " "
+		elif val == 1:
+			return "☻"
+		elif val == 2:
+			return "☺"
+		else:
+			return "?"
+		
